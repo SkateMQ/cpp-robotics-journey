4d6625e74055823d90a7add10cd3a0ef380d77e8
maself <maqiangsk8@gmail.com>
2026-05-02 16:19:36 +0800

    feat: do homework
    complete homework in LearnCpp.com Chapter 1.5

--- week01/io/cin.cpp
+++ week01/io/cin.cpp
@@ -2,25 +2,39 @@
 
 int main()
 {
-    std::cout << "Enter two numbers: ";
+    // std::cout << "Enter two numbers: ";
     
+    // int x{};
+    // int y{};
+
+    // std::cin >> x >> y;
+    // std::cout << "You entered " << x << " and " << y << "\n";
+
+    // std::cout << "Enter two numbers again: ";
+    
+    // int n{};
+
+    // std::cin >> n;
+
+    // int m{};
+
+    // std::cin >> m;
+
+    // std::cout << "You entered " << n << " and " << m << "\n";
+
+    // std::cout << "Enter a number: ";
+    // int x{};
+    // std::cin >> x;
+    // std::cout << "You entered " << x << "\n";
+
+    std::cout << "Enter three numbers: ";
+
     int x{};
     int y{};
+    int z{};
 
-    std::cin >> x >> y;
-    std::cout << "You entered " << x << " and " << y << "\n";
-
-    std::cout << "Enter two numbers again: ";
-    
-    int n{};
-
-    std::cin >> n;
-
-    int m{};
-
-    std::cin >> m;
-
-    std::cout << "You entered " << n << " and " << m << "\n";
+    std::cin >> x >> y >> z;
+    std::cout << "You entered: " << x << ", " << y << " and " << z << ".\n";
 
     return 0;
 }
